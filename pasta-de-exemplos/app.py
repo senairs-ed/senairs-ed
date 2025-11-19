ee3056ecb159f9911742376296a612557cdf29b4
Dyego Alves <dyego.alves@senairs.org.br>
2025-11-19 10:20:51 -0300

    add code de exemplo para teste

---
+++ pasta-de-exemplos/app.py
@@ -0,0 +1,10 @@
+# Função que soma dois números
+def somar(a, b):
+    return a + b
+
+# Testando a função
+numero1 = 5
+numero2 = 3
+resultado = somar(numero1, numero2)
+
+print(f"A soma de {numero1} e {numero2} é {resultado}.")
